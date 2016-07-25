69588596abee34cec215d6437d24335551a6ee30
Jack Carpenter <lynxster222@gmail.com>
2016-07-25 17:41:15 -0400

    blocking and open issues w Hart

--- js/project.js
+++ js/project.js
@@ -1,12 +1,21 @@
 // Your code here.
+//creating event for click on div.roster
+document.getElementById("roster").onclick = function(){
+	document.querySelector("roster").classList.toggle("open");
+};
+
+
+
+
+
 
 // FREEBIES! Blank-out the inputs onfocus.
-document.getElementById("name").onfocus = clearName;
-document.getElementById("grade").onfocus = clearGrade;
+//document.getElementById("name").onfocus = clearName;
+//document.getElementById("grade").onfocus = clearGrade;
 
-function clearName() {
-  document.getElementById("name").value = "";
-}
-function clearGrade() {
-  document.getElementById("grade").value = "";
-}
+//function clearName() {
+  //document.getElementById("name").value = "";
+//}
+//function clearGrade() {
+//  document.getElementById("grade").value = "";
+//}
