dd21764ef84ae7fda6fd463b29f08cc5e0659479
copper-blue <selab.edu@gmail.com>
2016-07-25 14:23:59 -0400

    Incremental Check-In

--- js/project.js
+++ js/project.js
@@ -1,12 +1,99 @@
-// Your code here.
+// Variable declarations
+var roster = document.getElementById("roster");
+var assign = document.getElementById("assign");
+var divRoster = document.querySelector("div.roster");
+var divAssign = document.querySelector("div.assign");
+var rosterList = document.getElementById("rosterList");
+var studentName = document.getElementById("name");
+var grade = document.getElementById("grade");
+var addButton = document.getElementById("clickme");
+var reportCard = document.getElementById("reportcard");
 
-// FREEBIES! Blank-out the inputs onfocus.
-document.getElementById("name").onfocus = clearName;
-document.getElementById("grade").onfocus = clearGrade;
+// Event Handlers
+roster.onclick = toggleRoster;
+assign.onclick = toggleAssign;
+addButton.onclick = addStudentGrade;
+studentName.onfocus = clearValue;
+grade.onfocus = clearValue;
 
-function clearName() {
-  document.getElementById("name").value = "";
+
+// Function declarations
+function clearValue() {
+  this.value = "";
 }
-function clearGrade() {
-  document.getElementById("grade").value = "";
+
+function toggleRoster() {
+	divRoster.style.display = "block";
+	divAssign.style.display = "none";
+
+	roster.classList = "selected";
+	assign.classList = "";
+
+	loadStudentRoster();
+}
+
+function toggleAssign() {
+	divRoster.style.display = "none";
+	divAssign.style.display = "block";
+
+	roster.classList = "";
+	assign.classList = "selected";
+}
+
+function addStudentGrade() {
+	//var student = { name: studentName.value, grade: grade.value };
+	var li = document.createElement("li");
+	var text = document.createTextNode(studentName.value + ", " + getLetterGrade(grade.value));
+	li.appendChild(text);
+	reportCard.appendChild(li);
+}
+
+function getLetterGrade(numGrade) {
+	var letterGrade;
+
+	if (isNaN(numGrade)) {
+		letterGrade = "WTF";
+	} else if (numGrade > 100) {
+		letterGrade = "A+";
+	} else if (numGrade >= 95) {
+		letterGrade = "A";
+	} else if (numGrade >= 90) {
+		letterGrade = "A-"
+	} else if (numGrade >= 87) {
+		letterGrade = "B+";
+	} else if (numGrade >= 84) {
+		letterGrade = "B";
+	} else if (numGrade >= 80) {
+		letterGrade = "B-";
+	} else if (numGrade >= 77) {
+		letterGrade = "C+";
+	} else if (numGrade >= 74) {
+		letterGrade = "C";
+	} else if (numGrade >= 70) {
+		letterGrade = "C-";
+	} else if (numGrade >= 67) {
+		letterGrade = "D+";
+	} else if (numGrade >= 64) {
+		letterGrade = "D";
+	} else if (numGrade >= 60) {
+		letterGrade = "D-";
+	} else {
+		letterGrade = "F";
+	}
+	
+	return letterGrade;
+}
+
+
+function loadStudentRoster() {
+	if (students) {
+		rosterList.innerHTML = "";
+
+		for (var i = 0; i <= students.length - 1; i++) {
+			var li = document.createElement("li");
+			var text = document.createTextNode(students[i].name);
+			li.appendChild(text);
+			rosterList.appendChild(li);
+		}
+	}
 }
