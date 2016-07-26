75b45cc683723881aa26d0f634a8941b7c46d126
caterina <carmenter@CArmenter-MacBook-Pro.local>
2016-07-26 18:03:00 -0400

    dropdown success

--- js/project.js
+++ js/project.js
@@ -1,5 +1,28 @@
 // Your code here.
+document.getElementById("roster").onclick = function(){
+	document.querySelector(".roster").classList.toggle("open");
+};
 
+document.getElementById("assign").onclick = function(){
+	document.querySelector(".assign").classList.toggle("open");
+};
+
+
+
+
+
+
+
+// FREEBIES! Blank-out the inputs onfocus.
+document.getElementById("name").onfocus = clearName;
+document.getElementById("grade").onfocus = clearGrade;
+
+function clearName() {
+  document.getElementById("name").value = "";
+}
+function clearGrade() {
+  document.getElementById("grade").value = "";
+}
 // FREEBIES! Blank-out the inputs onfocus.
 document.getElementById("name").onfocus = clearName;
 document.getElementById("grade").onfocus = clearGrade;
