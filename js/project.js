1becc55ef1e212bc7e15430a2ef611c4e78d6df8
sabgab <sabrina_gaber@yahoo.com>
2016-07-26 18:58:26 -0400

    Week 5 HW
    I tried to do this.  Was pretty difficult. couldn’t get the add button
    to work.

--- js/project.js
+++ js/project.js
@@ -1,6 +1,5 @@
 // Your code here.
 
-// FREEBIES! Blank-out the inputs onfocus.
 document.getElementById("name").onfocus = clearName;
 document.getElementById("grade").onfocus = clearGrade;
 
@@ -10,3 +9,27 @@ function clearName() {
 function clearGrade() {
   document.getElementById("grade").value = "";
 }
+
+document.getElementById("roster").onclick = function(){
+	document.querySelector("div.roster").classList.toggle("open");
+	document.querySelector("nav a").classList.toggle("open");
+};
+
+document.getElementById("assign").onclick = function(){
+	document.querySelector("div.assign").classList.toggle("open");
+	document.querySelector("nav a").classList.toggle("open");
+};
+
+//document.getElementById("reportcard").onclick = function (){
+//	document.querySelector().
+
+//function addgrade (){ 
+//document.getElementById("reportcard").innerHTML=.value;
+//};
+
+//document.getElementById("clickme").onclick = copy;
+
+
+// FREEBIES! Blank-out the inputs onfocus.
+
+
