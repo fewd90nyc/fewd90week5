cb6992dbcf33e2fb89a1815f3b4135ec0e4823e0
Jett H <jetthouse@Jetts-MacBook-Pro.local>
2016-07-26 18:34:18 -0400

    Added Grade functionality in JS
    Updated CSS slightly to remove classes for the two tabs (redundant).
    Added back in Display to DIV CSS.
    
    Added all of JS for converting grades and writing them on page. Full
    disclosure: would not have figured out how to write and preserve
    previous entries without coworkers. WTF, SO HARD.

--- js/project.js
+++ js/project.js
@@ -1,5 +1,7 @@
 // Your code here.
 
+//3 attempts at getting tabs to work
+
 /*
 document.getElementById("roster").onclick = function(){
  document.div.classList.toggle("roster");
@@ -39,8 +41,62 @@ document.getElementById("assign").onclick = function() {
 // 	};
 // };
 
+var grade;
+var letterGrade;
+var name;
+var existingLetterGrades = "";
 
+document.getElementById("clickme").onclick = addToReportcard
 
+function addToReportcard(){
+  grade = document.getElementById("grade").value;
+  name = document.getElementById("name").value;
+
+  if(grade >= 94) {
+    letterGrade = "A"
+  }
+  else if (grade >= 90) {
+    letterGrade = "A-"
+  }
+  else if (grade >= 87) {
+    letterGrade = "B+"
+  }
+  else if (grade >= 84) {
+    letterGrade = "B"
+  }
+  else if (grade >= 80) {
+    letterGrade = "B-"
+  }
+  else if (grade >= 77) {
+    letterGrade = "C+"
+  }
+  else if (grade >= 74) {
+    letterGrade = "C"
+  }
+  else if (grade >= 70) {
+    letterGrade = "C-"
+  }
+  else if (grade >= 67) {
+    letterGrade = "D+"
+  }
+  else if (grade >= 64) {
+    letterGrade = "D"
+  }
+  else if (grade >= 60) {
+    letterGrade = "D-"
+  }
+  else if (grade >= 0) {
+    letterGrade = "F"
+  }
+  else {
+    letterGrade = "Invalid Entry";
+  }
+  existingLetterGrades = (existingLetterGrades + "<li>"+name+", "+letterGrade+"</li>");
+  console.log(existingLetterGrades);
+  document.getElementById("reportcard").innerHTML = existingLetterGrades;
+};
+
+//"rgb("+red+","+green+","+blue+")";
 
 // FREEBIES! Blank-out the inputs onfocus.
 document.getElementById("name").onfocus = clearName;
