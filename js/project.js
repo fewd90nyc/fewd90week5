7cf8f61e48f05f857552c298d4dcc8e1646f5192
Jett H <jetthouse@Jetts-MacBook-Pro.local>
2016-07-26 00:08:34 -0400

    Tabs JS 3rd attempt, added minor CSS
    3 JS attempts to work tabs. First two commented out. Added CSS for the
    second one to see if classes needed empty selectors to populate
    .style.display functions. No luck.

--- js/project.js
+++ js/project.js
@@ -1,5 +1,45 @@
 // Your code here.
 
+
+document.getElementById("roster").onclick = function(){
+  document.querySelector(.roster).classList.toggle("open");
+};
+
+
+
+//document.getElementById("roster").onclick = function() {
+//	document.getElementsByClassName("roster").style.display = "block";
+//	document.getElementsByClassName("assign").style.display = "none";
+//};
+
+//document.getElementById("assign").onclick = function() {
+//	document.getElementsByClassName("roster").style.display = "none";
+//	document.getElementsByClassName("assign").style.display = "block";
+//};
+
+
+
+//var showRoster;
+//var showAssign;
+
+//function() {
+//	showRoster = getElementById("roster").onclick
+//	showAssign = getElementById("assign").onclick
+//
+//	if(showRoster == true) { 
+//		document.getElementById("roster").style.display = "block";
+//		document.getElementById("assign").style.display = "none";
+//	};
+//
+//	else if(showAssign == true) {
+//		document.getElementById("roster").style.display = "none";
+//		document.getElementById("assign").style.display = "block";
+//	};
+//};
+
+
+
+
 // FREEBIES! Blank-out the inputs onfocus.
 document.getElementById("name").onfocus = clearName;
 document.getElementById("grade").onfocus = clearGrade;
