8c90e4d7d7e1e7e1e1f2bce91a3cd1b65918430d
robertohernandez11 <roberto@Robertos-Air.us.sinet.si.edu>
2016-07-26 17:00:10 -0400

    need help

--- js/project.js
+++ js/project.js
@@ -1,4 +1,26 @@
 // Your code here.
+var name;
+var grade;
+var roster;
+var assign;
+
+function roster() {
+  	var roster = document.getElementById("roster").value;
+ 	document.getElementById("roster").innerHTML = roster;
+} 	
+
+
+document.getElementById("roster").onclick = function(){
+  roster = document.getElementById("roster").value;
+
+document.getElementById("grade").onchange = function(){
+  grade = document.getElementById("grade").value;
+
+  if (name >= 72) {
+  	message = "C-"
+  }
+  document.getElementById('roster').onclick = roster;
+  }; 
 
 // FREEBIES! Blank-out the inputs onfocus.
 document.getElementById("name").onfocus = clearName;
@@ -10,3 +32,8 @@ function clearName() {
 function clearGrade() {
   document.getElementById("grade").value = "";
 }
+  document.getElementById("clickme").onfocus = result;
+  if (name >= 72) {
+  	message = "c minus"
+  }
+};
