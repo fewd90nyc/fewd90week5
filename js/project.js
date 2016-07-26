52126a3e114164520058864cdf063d842602f76a
Jett H <jetthouse@Jetts-MacBook-Pro.local>
2016-07-26 00:58:11 -0400

    Giving up on tab JS toggling
    Unexpected token error. I don’t know what that means. Google says
    syntax?

--- js/project.js
+++ js/project.js
@@ -1,21 +1,21 @@
 // Your code here.
 
 
-document.getElementById("roster").onclick = function(){
-  document.div.classList.toggle("roster");
+//document.getElementById("roster").onclick = function(){
+//  document.div.classList.toggle("roster");
+//};
+
+
+
+document.getElementById("roster").onclick = function() {
+	document.querySelector(.roster).style.display = "";
+	document.querySelector(div.assign).style.display = "none";
 };
 
-
-
-//document.getElementById("roster").onclick = function() {
-//	document.querySelector(.roster).style.display = "block";
-//	document.querySelector(.assign).style.display = "none";
-//};
-
-//document.getElementById("assign").onclick = function() {
-//	document.querySelector(.roster).style.display = "none";
-//	document.querySelector(.assign).style.display = "block";
-//};
+document.getElementById("assign").onclick = function() {
+	document.querySelector(.roster).style.display = "none";
+	document.querySelector(div.assign).style.display = "";
+};
 
 
 
