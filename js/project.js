47d1841141e4faedf65b70a6557a02e88d455039
vdigregorio <vdigregorio@Valerias-MacBook-Air.local>
2016-07-26 18:21:27 -0400

    initial commit
    hw week 5

--- js/project.js
+++ js/project.js
@@ -1,6 +1,45 @@
-// Your code here.
+function addNew() {
+	var ul = document.getElementById("reportcard");
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(document.getElementById('name').value + ", " + letterGrade(document.getElementById('grade').value)));
+	ul.appendChild(li);
+}
 
-// FREEBIES! Blank-out the inputs onfocus.
+function letterGrade(percent) {
+
+	var mark;
+
+	if (percent < 60)
+		mark = 'F';
+	else if (percent < 70) 
+		mark = 'D';
+	else if (percent < 80)
+		mark = 'C';
+	else if (percent < 90)
+		mark = 'B';
+	else if (percent <= 100)
+		mark = 'A';
+
+	return mark;
+
+}
+
+function selectAssign() {
+	$(".roster").hide();
+	$(".assign").show();
+
+	document.getElementById('roster').removeAttribute('class', 'selected');
+	document.getElementById('assign').setAttribute('class', 'selected');
+}
+
+function selectRoster() {
+	$(".assign").hide();
+	$(".roster").show();
+
+	document.getElementById('assign').removeAttribute('class', 'selected');
+	document.getElementById('roster').setAttribute('class', 'selected');
+}
+
 document.getElementById("name").onfocus = clearName;
 document.getElementById("grade").onfocus = clearGrade;
 
