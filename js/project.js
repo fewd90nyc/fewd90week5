562ae2450bef77d299d8e08f0b391aec33a631ef
Katie Long <yl3239@columbia.edu>
2016-07-26 13:32:45 -0400

    Update project.js

--- js/project.js
+++ js/project.js
@@ -1,8 +1,4 @@
 // ALL THE FUNCTIONS
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/gh-pages
 // pull out Roster page
 function showRoster(){
 	document.getElementById("roster").className="selected";
@@ -93,10 +89,7 @@ document.getElementById("reportcard").innerHTML + "<li>" + name + "," + grade + 
 
 
 
-<<<<<<< HEAD
-=======
 
->>>>>>> origin/gh-pages
 // click to switch between roster and assignment
 document.getElementById("roster").onclick=showRoster;
 document.getElementById("assign").onclick=showAssign;
@@ -107,26 +100,3 @@ document.getElementById("grade").onfocus = clearGrade;
 
 // click to input values
 document.getElementById("clickme").onclick=getValue;
-<<<<<<< HEAD
-=======
-
-
-
-
-
-
-
-
-
-
-
-
-// Feel free to extrapolate or add any features you think are interesting. Add validation, go wild!
-
-// **CHALLENGE**: You'll notice there is an additional script in the `js` folder, `js/students.js`. 
-// In that script is an Array of student Objects. The challenge, for those of you who are feeling 
-// really confident in your JS ability, is to, in your HTML, delete what is currently in the `<div class="roster">`. 
-// Add that script to your HTML before the project.js and then, in project.js, loop through that array, 
-// and populate the roster, AS WELL AS assigning a grade in the `#reportcard` for each student. 
-// If you're feeling REALLY ambitious, think about how you would _delete_ one of the `<li>` (onclick for instance).
->>>>>>> origin/gh-pages
