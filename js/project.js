62ed340e4f266ed29527c79cd0bf4a83394282a8
Jett H <jetthouse@Jetts-MacBook-Pro.local>
2016-07-26 16:02:50 -0400

    Revisiting Tab JS Solution 2 (3x)
    Figured out that the problem with my second solution was simply the
    lack of quotes. Who knew! Also needed to add block to display function.

--- js/project.js
+++ js/project.js
@@ -1,41 +1,43 @@
 // Your code here.
 
-
-//document.getElementById("roster").onclick = function(){
-//  document.div.classList.toggle("roster");
-//};
+/*
+document.getElementById("roster").onclick = function(){
+ document.div.classList.toggle("roster");
+};
+*/
 
 
 
 document.getElementById("roster").onclick = function() {
-	document.querySelector(.roster).style.display = "";
-	document.querySelector(div.assign).style.display = "none";
+	document.querySelector(".roster").style.display = "block";
+	document.querySelector(".assign").style.display = "none";
 };
 
 document.getElementById("assign").onclick = function() {
-	document.querySelector(.roster).style.display = "none";
-	document.querySelector(div.assign).style.display = "";
+	document.querySelector(".roster").style.display = "none";
+	document.querySelector(".assign").style.display = "block";
 };
 
 
 
-//var showRoster;
-//var showAssign;
 
-//function() {
-//	showRoster = getElementById("roster").onclick
-//	showAssign = getElementById("assign").onclick
-//
-//	if(showRoster == true) { 
-//		document.getElementById("roster").style.display = "block";
-//		document.getElementById("assign").style.display = "none";
-//	};
-//
-//	else if(showAssign == true) {
-//		document.getElementById("roster").style.display = "none";
-//		document.getElementById("assign").style.display = "block";
-//	};
-//};
+// var showRoster;
+// var showAssign;
+
+// function() {
+// 	showRoster = getElementById("roster").onclick
+// 	showAssign = getElementById("assign").onclick
+
+// 	if(showRoster == true) { 
+// 		document.getElementById("roster").style.display = "block";
+// 		document.getElementById("assign").style.display = "none";
+// 	};
+
+// 	else if(showAssign == true) {
+// 		document.getElementById("roster").style.display = "none";
+// 		document.getElementById("assign").style.display = "block";
+// 	};
+// };
 
 
 
