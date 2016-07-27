e40a1bb1d6fd8244fb423f1434be7437759404b8
joycekwon <joyceyskwon@gmail.com>
2016-07-26 22:14:11 -0400

    changes in js and css
    fixed errors and “showAssign”

--- js/project.js
+++ js/project.js
@@ -7,10 +7,17 @@ function showRoster() {
 	document.querySelector("#roster").classList.toggle("selected");
 };
 
-document.getElementById("assign").onclick = function() {
+document.getElementById("roster").onclick = showRoster;
+
+
+
+function showAssign() {
 	document.querySelector(".assign").classList.toggle("open");
 	document.querySelector("#assign").classList.toggle("selected");
 };
+
+document.getElementById("assign").onclick = showAssign;
+
 
 document.getElementById("clickme").onclick = assignGrade;
 
@@ -28,39 +35,41 @@ function assignGrade() {
 	var student = document.getElementById("name").value;
 	var grade = document.getElementById("grade").value;
 
-	if (grade >= 0) {
+	if (grade < 60) {
 		grade = "F";
 	}
-	else if (grade >= 60) {
-		grade = "D-";
-	}
-	else if (grade >= 65) {
+	else if (grade < 70) {
 		grade = "D";
 	}
-	else if (grade >= 70) {
+	else if (grade < 75) {
 		grade = "C-";
 	}
-	else if (grade >= 70) {
-		grade = "C-";
-	}
-	else if (grade >= 75) {
+	else if (grade < 80) {
 		grade = "C";
 	}
-	else if (grade >= 80) {
+	else if (grade < 85) {
 		grade = "B-";
-	else if (grade >= 85) {
+	}
+	else if (grade < 90) {
 		grade = "B";
 	}
-	else if (grade >= 90) {
+	else if (grade < 95) {
 		grade = "A-";
-	else if (grade >= 95) {
+	}
+	else if (grade < 100) {
 		grade = "A";
+	}
+	else if (grade = 100) {
+		grade = "A"
 	}
 	else {
 		message = "Invalid Entry";
 	}
 }
 
+function assignGrade(student, grade) {
+	document.getElementById("reportcard").innerHTML = document.getElementById("reportcard").innerHTML+"<li>"+student+ "grade"+"</li>";
+}
 
 function clearName() {
   document.getElementById("name").value = "";
@@ -71,6 +80,7 @@ function clearName() {
 
 function clearGrade() {
   document.getElementById("grade").value = "";
+ }
 
 
 
