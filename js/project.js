a00b6691e4009701f96e72f255f912959a1714dc
Jack Carpenter <lynxster222@gmail.com>
2016-07-28 21:46:59 -0400

    frsh code

--- js/project.js
+++ js/project.js
@@ -1,7 +1,64 @@
-// Your code here.
-document.getElementById("roster").onclick = function(){
-  document.querySelector(".roster").classList.toggle("open");
-};
+// // Your code here.
+// document.getElementById("roster").onclick = function(){
+// document.querySelector(".roster").classList.toggle("open");
+// };
+
+// document.getElementById("assign").onclick = function(){
+// document.querySelector(".name").classList.toggle("open");
+// };
+
+document.getElementById("name").onfocus = clearName;
+document.getElementById("grade").onfocus = clearGrade;
+
+function clearGrade (){
+	document.getElementById("grade").value = "";
+}
+
+function clearName (){
+	document.getElementById("name").value + "";
+}
+
+function showRoster () {
+	document.querySelector(".roster").classList.toggle("open");
+	document.querySelector("#roster").classList.toggle("selected");
+	document.querySelector(".assign").classList.remove("open");
+	document.querySelector("#assign").classList.remove("selected");
+}
+
+document.getElementById("roster").onclick = showRoster;
+
+function showInputs() {
+	document.querySelector(".assign").classList.toggle("open");
+	document.querySelector("#assign").classList.toggle("selected");
+	document.querySelector(".roster").classList.remove("open");
+	document.querySelector("#roster").classList.remove("selected");
+}
+
+document.getElementById("assign").onclick = showInputs;
+
+document.getElementById("assign").onclick = addNameGrade;
+
+function addNameGrade () {
+
+	var student = document.getElementById("name").value;
+	var grade = document.getElementById("grade").value;
+
+	if (grade >= 90) {
+		grade = "A+";
+	}
+	else if (grade >= 85) {
+		grade = "A-";
+	}
+	else if (grade >= 80) {
+		grade = "B+";
+	}
+
+	document.getElementById("reportcard").innerHTML = document.getElementById("reportcard").innerHTML + "<li>"+ studentName+","+grade+"</li>";
+}
+
+
+
+
 
 
 
@@ -30,3 +87,10 @@ document.getElementById("roster").onclick = function(){
 //function clearGrade() {
 //  document.getElementById("grade").value = "";
 //}
+
+
+//roster.style.display = "block";
+
+//roster.style.display = "none"
+
+
